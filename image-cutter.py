67507dba086481348d1ace1621085969b0537074
RicoloveFeng <kakagou.mc@outlook.com>
2020-05-22 22:35:32 +0800

    add gap cutting

--- image-cutter.py
+++ image-cutter.py
@@ -5,11 +5,13 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("--mode", "-m", help="cutting mode", choices=["s","l"], default="s")
 parser.add_argument("--file", "-f", help="image path", required=True)
+parser.add_argument("--gap", "-g", help="consider gap when cutting", choices=[True, False], default=True)
 
 args = parser.parse_args()
 
 mode = args.mode
 file = args.file
+gap = args.gap
 
 image = Image.open(file)
 imageWidth = image.size[0]
@@ -17,6 +19,8 @@ imageHeight = image.size[1]
 fileFormat = file.split(".")[-1]
 folder = "./" + os.path.basename(file)[:-len(fileFormat)-1] + "/"
 
+if not os.path.exists(folder):
+    os.makedirs(folder)
 
 if mode == "s":
     x1 = max (imageWidth - imageHeight, 0) // 2
@@ -37,23 +41,16 @@ imageWidth = image.size[0]
 imageHeight = image.size[1]
 dw = imageWidth // 3
 dh = imageHeight // 3
-
-if not os.path.exists(folder):
-    os.makedirs(folder)
+gapValue = int(imageWidth / 422 * 6) // 2 if gap else 0
 
 for i in range(3):
     for j in range(3):
         subimg = image.crop(
             (
-                dw * j,
-                dh * i,
-                min(dw * (j + 1), imageWidth),
-                min(dh * (i + 1), imageHeight)
+                dw * j + gapValue * min(j, 1),
+                dh * i + gapValue * min(i, 1),
+                min(dw * (j + 1), imageWidth) - gapValue * min(2 - j, 1),
+                min(dh * (i + 1), imageHeight) - gapValue * min(2 - i, 1)
             )
         )
         subimg.save(folder +str(i*3 + j + 1) + "." + fileFormat)
-
-
-
-
-
